419fb07be44cf090b86484024d2f9f34f092dc1d
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2023-11-17 20:25:48 +0530

    Username generator completed

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 import json
 
 f = open('words.json')
+g = open('output.txt', 'a')
 data = json.load(f)
 
-for word in data:
-    print(word)
+size = int(input("Enter size of username required: "))
+for i in range(0, len(data)):
+    if len(data[i]) == size:
+        g.write(data[i] + "\n")
+        continue
+    elif len(data[i]) > size:
+        continue
+    for j in range(i+1, len(data)):
+        if len(data[j]) == size:
+            g.write(data[j] + "\n")
+            continue
+        elif len(data[j]) > size:
+            continue
+        elif len(data[i] + data[j]) == size:
+            g.write(data[i] + data[j] + "\n")
+            g.write(data[j] + data[i] + "\n")
 
 f.close()
+g.close()
